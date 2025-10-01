6263dafcab354b1e1eaf2291311cb00fbf2c09ea
macvg2025 <macvg2025@outlook.com>
2025-09-30 19:47:38 -0700

    Update script.js

--- click/script.js
+++ click/script.js
@@ -22,3 +22,25 @@ function initGame() {
     displayAchievements();
 }
 
+function displayUpgrades() {
+    const upgradesList = document.getElementById('upgrades-list');
+    upgradesList.innerHTML = ''; // Clear anything already there
+
+    gameData.clickUpgrades.forEach((upgrade, index) => {
+        const div = document.createElement('div');
+        div.className = 'upgrade';
+        div.innerHTML = `
+            <h3>${upgrade.name}</h3>
+            <p>Cost: ${upgrade.cost}</p>
+            <p>Effect: ${upgrade.effect}</p>
+            <button onclick="buyUpgrade(${index})">Buy</button>
+        `;
+        upgradesList.appendChild(div);
+    });
+}
+
+function buyUpgrade(index) {
+    const upgrade = gameData.clickUpgrades[index];
+    console.log('Bought upgrade:', upgrade.name);
+    // Here you would subtract cost, add multiplier, trigger animation, etc.
+}
